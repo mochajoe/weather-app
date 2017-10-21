756850505ad10ed8398cb1c3d1a24995db5867ee
Anthony Nguyen <anguyee@gmu.edu>
2017-10-21 03:47:11 -0700

    basic implimentation working and created 3 functions, One to capitalized each first letter of a string, another to convert Kelvins to Fehrenheits, and function that takes the classname and what you would like to output to the class and displays it to the DOM

--- javascript.js
+++ javascript.js
@@ -7,11 +7,34 @@ fetch(`${weatherApi}ip`)
 		return response.json();
 	})
 	.then(data => {
-		console.log(data);
 		city = data.city;
 		country = data.country;
 		latitude = data.location.latitude;
 		longitude = data.location.longitude;
+		displayToDom("location", city);
+		fetch(`${weatherApi}weather/${latitude},${longitude}`)
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				let kelvinTemp = data.main.temp;
+				let condition = data.weather[0].description;
+				displayToDom("status", kToF(kelvinTemp));
+				displayToDom("condition", capitalizeEachWord(condition));
+			});
 	});
 
-document.getElementsByClassName("location");
+kToF = Fahrenheit => {
+	return parseInt((Fahrenheit - 273.15) * 1.8 + 32) + "&#8457;";
+};
+
+capitalizeEachWord = str => {
+	return str
+		.split(" ")
+		.map(word => word.toUpperCase()[0] + word.slice(1))
+		.join(" ");
+};
+
+displayToDom = (className, data) => {
+	return (document.getElementsByClassName(className)[0].innerHTML = data);
+};
