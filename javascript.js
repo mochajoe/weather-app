5f3e618f2c7228dc1a610ac5867870646c7b36ef
Anthony Nguyen <anguyee@gmu.edu>
2017-10-21 01:49:20 -0700

    implimented fetch for api call

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
+let weatherApi = "https://weathersync.herokuapp.com/";
+
+let city, country, latitude, longitude;
+
+fetch(`${weatherApi}ip`)
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		console.log(data);
+		city = data.city;
+		country = data.country;
+		latitude = data.location.latitude;
+		longitude = data.location.longitude;
+	});
+
 document.getElementsByClassName("location");
