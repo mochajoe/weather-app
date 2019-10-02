8ece1af2168ebf5cf7bdc1dfe49dbdbbb71d226a
Lala <jieyingpeng010@gmail.com>
2019-10-01 22:41:16 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -77,6 +77,7 @@ const getLocation = obj => {
       weatherData.kelvinTemp = data.main.temp;
       weatherData.condition = data.weather[0].description;
       weatherData.icon = data.weather[0].icon;
+      weatherData.zipcode = data.weather[0].zipcode;
       weatherData.currentConditionsFor = "CURRENT CONDITIONS FOR:";
       renderToDom(locationObj.city, weatherData);
     })
